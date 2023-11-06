20a0c12880f6f49b3d1cb71e4e40224c17315606
Rajeshaachari <107452668+RajeshAachari@users.noreply.github.com>
2023-11-06 23:28:01 +0530

    Update HelloWorldBacking.java

--- src/main/java/com/ranjitswain/HelloWorldBacking.java
+++ src/main/java/com/ranjitswain/HelloWorldBacking.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.ranjitswain;
+package com.rajeshaachari;
 
 /**
  * A typical simple backing bean, that is backed to <code>devOps.jsp</code>
